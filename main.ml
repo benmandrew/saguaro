5c437ea5db5283a71c01275efe78b57be4732ea6
Mirrorworld <ba405@cam.ac.uk>
2021-03-13 23:36:20 +0000

    in progress of adding pure literals

--- main.ml
+++ main.ml
@@ -5,6 +5,21 @@ type literal = {
   sign : bool;
 }
 
+module Set = Set.Make(
+  struct
+    let compare l1 l2 =
+      match Char.compare l1.lit l2.lit with
+      | 0 ->
+        if l1.sign && not l2.sign then 1
+        else if not l1.sign && l2.sign then -1
+        else 0
+      | v -> v
+    type t = literal
+  end)
+
+let inv l =
+  {lit=l.lit; sign=not l.sign}
+
 let isEmpty clause =
   match clause with
   | [] -> true
@@ -54,6 +69,33 @@ let rec printList = function
     print_char x.lit ; print_string " " ; printList l
 
 
+let rec getPureLiteralsClause (first, second) clause =
+  match clause with
+  | [] -> (first, second)
+  | l::clause ->
+    if Set.mem (inv l) second then
+      getPureLiteralsClause (first, second) clause else
+    if Set.mem l first then
+      (first, Set.add (inv l) second) else
+    (Set.add l first, second)
+
+let pureLiteralsAux (first, second) clauses =
+  match clauses with
+  | [] -> (first, second)
+  | clause::clauses ->
+    fold_left (
+      fun (first, second) x ->
+        getPureLiteralsClause (first, second) x)
+      clause
+
+let pureLiterals clauses =
+  let first = Set.empty in
+  let second = Set.empty in
+  match pureLiteralsAux (first, second) clauses with
+  | (first, second) ->
+    Set.filter (fun x -> not Set.mem x second) first
+
+
 let rec dpll clauses =
   List.iter printList clauses ; print_string "\n" ;
   if List.exists isEmpty clauses then false
@@ -62,7 +104,8 @@ let rec dpll clauses =
   | None -> true
 
 let _ =
-  dpll [
+  (* dpll [ *)
+  let (first, second) = pureLiterals [
     [
       {lit = 'A'; sign = true}
     ];
@@ -70,5 +113,12 @@ let _ =
       {lit = 'A'; sign = false};
       {lit = 'B'; sign = true};
       {lit = 'C'; sign = true}
+    ];
+    [
+      {lit = 'A'; sign = true};
+      {lit = 'B'; sign = false};
+      {lit = 'C'; sign = false}
     ]
-  ]
+  ] in
+  List.iter (fun x -> printList x) first;
+  List.iter (fun x -> printList x) second;
