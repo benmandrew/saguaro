83aa2561f2727708acecfbc7c12c7a6cd23a6f03
Mirrorworld <ba405@cam.ac.uk>
2021-03-16 01:11:00 +0000

    Pure literal elimination working

--- main.ml
+++ main.ml
@@ -44,9 +44,8 @@ let rec unitPropagateAux clause lit =
   | [] -> []
   | l::clause when l.lit <> lit.lit ->
     l::(unitPropagateAux clause lit)
-  | l::clause ->
-    if l.sign == lit.sign then []
-    else unitPropagateAux clause lit
+  | l::_ when l.sign == lit.sign -> []
+  | _::clause -> unitPropagateAux clause lit
 
 let unitPropagate lit clause =
   match unitPropagateAux clause lit with
@@ -62,63 +61,79 @@ let rec filterMapAux l =
 let filterMap f l =
   filterMapAux (List.map f l)
 
-let rec printList = function
-  | [] -> print_string "\n" ; ()
-  | x::l ->
-    if not x.sign then print_string "¬" ;
-    print_char x.lit ; print_string " " ; printList l
+let rec foldLeft f a l =
+  match l with
+  | [] -> a
+  | h::t -> foldLeft f (f a h) t
 
 
 let rec getPureLiteralsClause (first, second) clause =
   match clause with
   | [] -> (first, second)
+  | l::clause when Set.mem l second ->
+    getPureLiteralsClause (first, second) clause
+  | l::clause when Set.mem (inv l) first ->
+    getPureLiteralsClause (first, Set.add l second) clause
   | l::clause ->
-    if Set.mem (inv l) second then
-      getPureLiteralsClause (first, second) clause else
-    if Set.mem l first then
-      (first, Set.add (inv l) second) else
-    (Set.add l first, second)
+    getPureLiteralsClause (Set.add l first, second) clause
 
 let pureLiteralsAux (first, second) clauses =
-  match clauses with
-  | [] -> (first, second)
-  | clause::clauses ->
-    fold_left (
-      fun (first, second) x ->
-        getPureLiteralsClause (first, second) x)
-      clause
+  foldLeft (
+    fun (first, second) x ->
+      getPureLiteralsClause (first, second) x)
+    (first, second)
+    clauses
 
 let pureLiterals clauses =
-  let first = Set.empty in
-  let second = Set.empty in
-  match pureLiteralsAux (first, second) clauses with
+  match pureLiteralsAux (Set.empty, Set.empty) clauses with
   | (first, second) ->
-    Set.filter (fun x -> not Set.mem x second) first
+    let second = Set.map (fun x -> {lit=x.lit; sign=(not x.sign)}) second in
+    Set.filter (fun x -> not (Set.mem x second)) first
+
+let pureLitElim pure clauses =
+  foldLeft (fun clauses pureLit ->
+      List.map (fun clause ->
+          List.filter (fun x -> x <> pureLit) clause
+      ) clauses
+  ) clauses (Set.elements pure)
 
 
 let rec dpll clauses =
-  List.iter printList clauses ; print_string "\n" ;
-  if List.exists isEmpty clauses then false
+  if List.exists isEmpty clauses then clauses
   else match hasUnitClause clauses with
   | Some x -> dpll (filterMap (unitPropagate x) clauses)
-  | None -> true
+  | None ->
+    let pure = pureLiterals clauses in
+    match Set.elements pure with
+    | [] -> clauses (* TODO SPLIT CASE *)
+    | _ -> dpll (pureLitElim pure clauses)
+
+
+let rec printList = function
+  | [] -> print_string "\n"
+  | x::l ->
+    if not x.sign then print_string "¬" ;
+    print_char x.lit ; print_string " " ; printList l
+
 
 let _ =
-  (* dpll [ *)
-  let (first, second) = pureLiterals [
-    [
-      {lit = 'A'; sign = true}
-    ];
+  let clauses = [
+    (* [
+      {lit = 'A'; sign = true};
+    ]; *)
     [
       {lit = 'A'; sign = false};
       {lit = 'B'; sign = true};
-      {lit = 'C'; sign = true}
+      {lit = 'C'; sign = false};
     ];
     [
       {lit = 'A'; sign = true};
       {lit = 'B'; sign = false};
-      {lit = 'C'; sign = false}
+      {lit = 'D'; sign = true};
     ]
   ] in
-  List.iter (fun x -> printList x) first;
-  List.iter (fun x -> printList x) second;
+  let clauses' = dpll clauses in
+  List.iter (fun x -> printList x) clauses ;
+  print_char '\n' ;
+  List.iter (fun x -> printList x) clauses' ;
+  print_char '\n' ;
