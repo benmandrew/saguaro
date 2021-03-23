151a53ae0c184f4ebac87656187e609e8afa133e
benmandrew <ba405@cam.ac.uk>
2021-03-23 12:43:56 +0000

    CNF DPLL fully working

--- main.ml
+++ main.ml
@@ -39,14 +39,6 @@ let rec hasUnitClause clauses =
     | None -> hasUnitClause clauses
     | Some x -> Some x
 
-let rec unitPropagate lit clause =
-  match clause with
-  | [] -> []
-  | l::clause when l.lit <> lit.lit ->
-    l::(unitPropagate lit clause)
-  | l::_ when l.sign == lit.sign -> []
-  | _::clause -> unitPropagate lit clause
-
 let rec foldLeft f a l =
   match l with
   | [] -> a
@@ -94,13 +86,16 @@ let pureLiterals clauses =
     let second = Set.map (fun x -> {lit=x.lit; sign=(not x.sign)}) second in
     Set.filter (fun x -> not (Set.mem x second)) first
 
-let pureLitElim pure clauses =
-  foldLeft (fun clauses pureLit ->
-      List.map (fun clause ->
-          List.filter (fun x -> x <> pureLit) clause
-      ) clauses
-  ) clauses (Set.elements pure)
-
+let assign lit (clauses, assignments) =
+  let clauses' = List.map (fun clause ->
+    if List.mem lit clause then None
+    else Some (List.filter (fun el -> el <> (inv lit)) clause))
+    clauses
+  |> List.filter (fun x -> x <> None)
+  |> List.map (fun x -> match x with
+    | None -> raise (Failure "List contains None after filtering")
+    | Some x -> x) in
+  (clauses', Set.add lit assignments)
 
 let printLiteral lit =
   if not lit.sign then print_string "¬" ;
@@ -117,59 +112,66 @@ let printList l =
 let printClauses clauses =
   List.iter (fun x -> printList x) clauses
 
-let rec dpllAux assignments clauses =
-  printClauses clauses;
-  if isEmpty clauses then
-    (print_string "No clauses:\n"; printClauses clauses; print_char '\n';
-    Set.empty) else
-  if List.exists isEmpty clauses then
-    (print_string "Empty clause:\n"; printClauses clauses; print_char '\n';
-    assignments) else
+let rec dpllAux clauses assignments =
+  if isEmpty clauses then assignments else
+  if List.exists isEmpty clauses then Set.empty else
   match hasUnitClause clauses with
   | Some x ->
-    print_string "Unit clause:\n"; printClauses clauses; print_char '\n';
-    dpllAux (Set.add x assignments) (List.map (unitPropagate x) clauses)
+    let (clauses, assignments) = assign x (clauses, assignments) in
+    dpllAux clauses assignments
   | None -> (
     let pure = pureLiterals clauses in
     match Set.elements pure with
     | _::_ -> (
-      print_string "Pure literals:\n"; printClauses clauses; print_char '\n';
-      dpllAux (Set.union assignments pure) (pureLitElim pure clauses))
+      let (clauses, assignments) = Set.fold assign pure (clauses, assignments) in
+      dpllAux clauses assignments)
     | [] -> (
       let split = List.hd (List.hd clauses) in
-      print_string "Split left:\n"; printLiteral split ; print_char '\n' ; printClauses clauses; print_char '\n';
-      let left = dpllAux (Set.add split assignments) (List.map (unitPropagate split) clauses) in
+      let (clauses, assignments) = assign split (clauses, assignments) in
+      let left = dpllAux clauses assignments in
       match Set.elements left with
       | _::_ -> left
       | [] -> (
-        print_string "Split right:\n"; printClauses clauses; print_char '\n';
-        let right = dpllAux (Set.add (inv split) assignments) (List.map (unitPropagate (inv split)) clauses) in
+        let (clauses, assignments) = assign split (clauses, assignments) in
+        let right = dpllAux clauses assignments in
         match Set.elements right with
         | _::_ -> right
         | [] -> Set.empty)))
 
 let dpll clauses =
-  Set.elements (dpllAux Set.empty clauses)
+  Set.elements (dpllAux clauses Set.empty)
 
 let _ =
   let clauses = [
-    (* [
-      {lit = 'A'; sign = true};
-    ]; *)
     [
       {lit = 'A'; sign = false};
-      {lit = 'B'; sign = true};
-      {lit = 'C'; sign = false};
+      {lit = 'D'; sign = false};
+      {lit = 'E'; sign = true};
+    ];
+    [
+      {lit = 'A'; sign = false};
+      {lit = 'F'; sign = true};
+      {lit = 'E'; sign = false};
+    ];
+    [
+      {lit = 'A'; sign = false};
+      {lit = 'F'; sign = false};
+      {lit = 'G'; sign = true};
+    ];
+    [
+      {lit = 'A'; sign = false};
+      {lit = 'G'; sign = false};
+      {lit = 'E'; sign = false};
     ];
     [
       {lit = 'A'; sign = true};
-      {lit = 'B'; sign = false};
       {lit = 'D'; sign = true};
+      {lit = 'F'; sign = true};
     ];
   ] in
   let model = dpll clauses in
-  (* printClauses clauses;
-  print_char '\n' ; *)
+  printClauses clauses;
+  print_char '\n' ;
   if model == [] then print_string "No satisfying model found\n"
   else (
     print_string "Model found:\n";
